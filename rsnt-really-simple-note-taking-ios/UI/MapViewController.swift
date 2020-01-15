5ff0b9c2b402256c1463b40245b7dd837722772b
Morgan Schuler <morganlschuler@gmail.com>
2020-01-15 14:56:35 -0800

    added MapView file

---
+++ rsnt-really-simple-note-taking-ios/UI/MapViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MapViewController.swift
+//  rsnt-really-simple-note-taking-ios
+//
+//  Created by Morgan Schuler on 1/15/20.
+//  Copyright © 2020 Németh László Harri. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

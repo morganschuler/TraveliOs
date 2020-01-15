67d979a18e4bf0d67a72847663d3ee3c8af04992
Morgan Schuler <morganlschuler@gmail.com>
2020-01-15 14:57:55 -0800

    added MapView file

--- rsnt-really-simple-note-taking-ios/UI/MapViewController.swift
+++ rsnt-really-simple-note-taking-ios/UI/MapViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 import MapKit
 
 class MapViewController: UIViewController {
-
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
